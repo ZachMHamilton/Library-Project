8ff21c35d376080cefc424a1bab19ea113cee0df
Zach Hamilton <zachmhamilton@outlook.com>
2023-08-15 14:07:26 -0400

    fixed sizing for bookList

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import reactDom from 'react-dom';
+import { createRoot } from 'react-dom';
 import App from './App.jsx';
 
-reactDom.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App />);
