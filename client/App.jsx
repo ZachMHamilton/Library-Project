a0b3e796e814af8d5f3ccd6fd2353be35471af65
Zach Hamilton <zachmhamilton@outlook.com>
2023-08-16 11:54:50 -0400

    conditional routing

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import MainContainer from './containers/MainContainer.jsx';
 import Login from './LoginPage/Login.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 
 const App = () => {
+  const loggedIn = false;
+
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<MainContainer />} />
+          <Route
+            path="/"
+            element={
+              loggedIn ? <MainContainer /> : <Navigate replace to={'/login'} />
+            }
+          />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
